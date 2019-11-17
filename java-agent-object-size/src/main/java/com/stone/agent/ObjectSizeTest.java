e379af6f944bf7132fd238d4ffdc4509a333e991
Donghua <shidonghua@meituan.com>
2019-11-17 22:26:57 +0800

    object size calculate demo

--- java-agent-object-size/src/main/java/com/stone/agent/ObjectSizeTest.java
+++ java-agent-object-size/src/main/java/com/stone/agent/ObjectSizeTest.java
@@ -5,25 +5,53 @@ import java.lang.instrument.Instrumentation;
 public class ObjectSizeTest {
     private Instrumentation inst;
 
+    /**
+     * -XX:+UseCompressedOops: mark/8 + Klass/8  = 16
+     * -XX:-UseCompressedOops: mark/8 + Klass/4  + padding/4 = 16
+     */
     static class X1 {
     }
 
+    /**
+     * -XX:+UseCompressedOops: mark/8 + Klass/4  + a/4 = 16
+     * -XX:-UseCompressedOops: mark/8 + Klass/8 + a/4 + padding/4 = 24
+     */
     static class X2 {
-        int a;
+        int a = 0;
     }
 
+    /**
+     * -XX:+UseCompressedOops: mark/8 + Klass/4  + i/4 = 16
+     * -XX:-UseCompressedOops: mark/8 + Klass/8 + i/8 = 24
+     */
     static class X3 {
-        Integer i;
-        Long l;
-        Double d;
+        Integer i = new Integer(0);
     }
 
+    /**
+     * -XX:+UseCompressedOops: mark/8 + Klass/4  + size/4  = 16
+     * -XX:-UseCompressedOops: mark/8 + Klass/8 + size/4 + padding/4 = 24
+     */
     static class X4 {
         int[] a = new int[100];
     }
 
+    /**
+     * -XX:+UseCompressedOops: mark/8 + Klass/4  + size/4  = 16
+     * -XX:-UseCompressedOops: mark/8 + Klass/8 + size/4 + padding/4 = 24
+     */
     static class X5 {
-        Integer[] a;
+        Integer[] a = new Integer[100];
+    }
+
+    /**
+     * -XX:+UseCompressedOops: mark/8 + Klass/4  + i/4 + l/8 + d/8  = 32
+     * -XX:-UseCompressedOops: mark/8 + Klass/8 + i/4 + l/8 + d/8 + padding/4= 40
+     */
+    static class X6 {
+        int i = 0;
+        long l = 0;
+        double d = 0;
     }
 
     public ObjectSizeTest(Instrumentation inst) {
@@ -31,12 +59,17 @@ public class ObjectSizeTest {
     }
 
     public void test() {
+        /**
+         * -XX:+UseCompressedOops: mark/8 + Klass/8  = 16
+         * -XX:-UseCompressedOops: mark/8 + Klass/4  + padding/4 = 16
+         */
         System.out.println("X0:" + inst.getObjectSize(new Object()));
         System.out.println("X1:" + inst.getObjectSize(new X1()));
         System.out.println("X2:" + inst.getObjectSize(new X2()));
         System.out.println("X3:" + inst.getObjectSize(new X3()));
         System.out.println("X4:" + inst.getObjectSize(new X4()));
         System.out.println("X5:" + inst.getObjectSize(new X5()));
+        System.out.println("X6:" + inst.getObjectSize(new X6()));
     }
 
     public static void main(String[] args) {
--- java-agent-test/src/main/java/com/stone/BootStrap.java
+++ java-agent-test/src/main/java/com/stone/BootStrap.java
@@ -1,10 +1,17 @@
 package com.stone;
 
 
+import java.io.IOException;
 import java.util.Arrays;
 
 public class BootStrap {
     public static void main(String[] args) {
         System.out.println("args = [" + Arrays.toString(args) + "]");
+
+/*        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
 }
